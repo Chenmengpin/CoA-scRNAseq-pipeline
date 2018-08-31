000e988cd26c3b203476e1311a524c50da4db193
jamesrhowe <j3howe@ucsd.edu>
2018-08-31 14:15:22 -0700

    Removed edgeR.

--- DifferentialGeneExpression.R
+++ DifferentialGeneExpression.R
@@ -1,103 +1,41 @@
 # Figures out differential gene expression between all different clusters
 
-# does differential gene expression analysis
-DGE_edgeR <- function(q_array, cluster_id) {
-  DEG_PValue <- list()
-  DEG_logFC <- list()
-  q_array <- 2^q_array
-  q_array <- q_array - 1  # removes the log2 normalization, converting into gene-summarized counts for edgeR
-  q_array <- t(q_array)
-  q_array[q_array < 0] <- 0   # if there are negative values, it will crash. all negatives are near-zero values from normalization
-  DGE_array <- DGEList(counts = q_array, group = cluster_id)
-  DGE_array <- calcNormFactors(DGE_array)
-  print("Beginning model fitting")
-  cell_det_rate <- scale(colMeans(q_array > 0))
-  genewise_models <- model.matrix(~ cell_det_rate + cluster_id)
-  model <- estimateDisp(DGE_array, design = genewise_models, robust = TRUE)
-  fit_model <- glmQLFit(model, design = genewise_models, robust = TRUE)
-  print("Model fitting done")
-  for (i in 1:nlevels(cluster_id)) {
-    DEresults_logFC <- data.frame(rownames(q_array))
-    rownames(DEresults_logFC) <- DEresults_logFC[,1]
-    DEresults_PValue <- data.frame(rownames(q_array))
-    rownames(DEresults_PValue) <- DEresults_PValue[,1]
-    DEresults_coln <- "Gene"
-    print(paste("Set of comparisons for cluster", i))
-    selected_cluster <- as.character(i)
-    for (j in 1:nlevels(cluster_id)) {
-      print(paste("Comparison against cluster", j))
-      if (j == selected_cluster) { next }
-      contrast <- numeric(ncol(genewise_models))
-      contrast[i] <- 1
-      contrast[j] <- -1
-      print("Running tests")
-      qlf_test <- glmQLFTest(fit_model, contrast = contrast)
-      print("Tests done")
-      newCompPV <- as.vector(qlf_test$table$PValue)
-      DEresults_PValue <- cbind.data.frame(DEresults_PValue, newCompPV)
-      newCompFC <- as.vector(qlf_test$table$logFC)
-      DEresults_logFC <- cbind.data.frame(DEresults_logFC, newCompFC)
-      DEresults_coln <- c(DEresults_coln, paste0(i, "_vs_", j))
-    }
-    colnames(DEresults_PValue) <- DEresults_coln
-    DEresults_PValue <- DEresults_PValue[,-1] 
-    colnames(DEresults_logFC) <- DEresults_coln
-    DEresults_logFC <- DEresults_logFC[,-1] 
-    for (m in 1:nrow(DEresults_PValue)) {
-      print(m)
-      DEresults_PValue[m,] <- p.adjust(DEresults_PValue[m,], method = "BH")
-    }
-    DEresults_PValue$SigComps <- rowSums(DEresults_PValue < .05)  
-    DEG_PValue[[i]] <- DEresults_PValue
-    names(DEG_PValue)[i] <- paste0("PValue_Cluster_", i)
-    DEG_logFC[[i]] <- DEresults_logFC
-    names(DEG_logFC)[i] <- paste0("logFC_Cluster_", i)
-  }
-  assign("DEG_PValue", DEG_PValue, env = .GlobalEnv)
-  assign("DEG_logFC", DEG_logFC, env = .GlobalEnv)
-  assign("DEG_model", model, env = .GlobalEnv)
-  assign("DEG_QLfitting", fit_model, env = .GlobalEnv)
-}
 
 # does differential gene expression analysis via MAST
 DGE_MAST <- function(q_array, cluster_id) {
+  MAST_clusters <- as.character(c(cluster_id, 0))   # sequester intercept away from real data, where we have no logFC for this
+  intercept_vector <- rep(0, times = length(colnames(q_array))) # treat intercept vector like a cell with its own cluster
+  MAST_array <- rbind.data.frame(q_array, intercept_vector)
   MAST_DGE_list <- list()
-  MAST_DGE_full <- list()
-  MAST_array <- t(q_array)
-  cell_det_rate <- scale(colMeans(MAST_array > 0))
+  MAST_array <- t(MAST_array)   # need this to format for analysis via MAST
+  cell_det_rate <- scale(colMeans(MAST_array > 0))  # CDR depends on finding the mean number of genes detected per cell
   MAST_sca <- FromMatrix(exprsArray = MAST_array, 
-                         cData = data.frame(wellKey = colnames(MAST_array),
-                                            clusters = cluster_id, cdr = cell_det_rate),
-                         fData = data.frame(primerid = rownames(MAST_array), 
-                                            Gene = rownames(MAST_array)))
-  zlm_result <- zlm(~cdr + clusters, sca = MAST_sca)
-  for (i in 1:length(colnames(zlm_result@coefC))) {
-    contrast_value <- colnames(zlm_result@coefC)[i]
-    LRT_array <- summary(zlm_result, doLRT = contrast_value)
-    LRT_dt <- LRT_array$datatable
-    LRT_dt <- LRT_dt[LRT_dt$contrast == contrast_value]
-    P_values <- LRT_dt[LRT_dt$component == "H",]
-    P_values$`Pr(>Chisq)` <- p.adjust(P_values$`Pr(>Chisq)`, method = "BH")
+                         fData = data.frame(primerid = rownames(MAST_array)), # primerid is what MAST calls the names of genes
+                         cData = data.frame(wellKey = colnames(MAST_array), # wellKey is what MAST calls cells
+                                            cluster = MAST_clusters, cdr = cell_det_rate))  # these are the co-variates of interest
+  zlm_result <- zlm(~cdr + cluster, sca = MAST_sca)   # need to compute the model for MAST
+  for (i in 1:(length(colnames(zlm_result@coefC) - 2))) {   # need to do this to prevent namelength errors
+    contrast_value <- colnames(zlm_result@coefC)[i+2]   # first two slots filled by the CDR and intercept, which are not going to be considered
+    LRT_array <- summary(zlm_result, doLRT = contrast_value)    # this calculates the p-values for each
+    LRT_dt <- LRT_array$datatable   # removes the superficial list layer
+    LRT_dt <- LRT_dt[LRT_dt$contrast == contrast_value]   # limits analysis to the cluster of interest
+    P_values <- LRT_dt[LRT_dt$component == "H",]    # use the p-value for the hurdle model
+    P_values$`Pr(>Chisq)` <- p.adjust(P_values$`Pr(>Chisq)`, method = "BH")   # need to correct P-values, else way too likely to find signficace
     gene_list <- P_values$primerid
     pval_list <- P_values$`Pr(>Chisq)`
-    P_values <-  P_values[P_values$`Pr(>Chisq)` < .01,]
-    LFC <- LRT_dt[LRT_dt$component == "logFC",]
+    LFC <- LRT_dt[LRT_dt$component == "logFC",]   # limits to the log-fold change
     LFC_list <- LFC$coef
-    LFC <- LFC[LFC$coef > .05,]
-    filtered_array <- cbind.data.frame(gene_list, pval_list, LFC_list)
+    filtered_array <- cbind.data.frame(gene_list, pval_list, LFC_list)  # this is the array we actually care about
     colnames(filtered_array) <- c("Gene", "Pvalue", "logFC")
     filtered_array$logFC[is.nan(filtered_array$logFC)] <- 0
-    filtered_array$PvalSig <- filtered_array$Pvalue < .01
-    filtered_array$logFCSig <- filtered_array$logFC > .05
-    markers <- data.frame(match(P_values$primerid, LFC$primerid), row.names = P_values$primerid)
-    markers <- rownames(markers)
-    MAST_DGE_list[[i]] <- markers
-    MAST_DGE_full[[i]] <- filtered_array
+    filtered_array$PvalSig <- filtered_array$Pvalue < .01   # cuts off significant p-value at FDR-corrected 0.01
+    filtered_array$logFCSig <- filtered_array$logFC > 1   # sets baseline for minimum increase to be considered a marker gene
+    filtered_array$marker <- filtered_array$PvalSig & filtered_array$logFCSig == TRUE   # unifies marker gene criteria
+    MAST_DGE_list[[i]] <- filtered_array
     print(i)
   }
-  names(MAST_DGE_list) <- colnames(zlm_result@coefC)
-  names(MAST_DGE_full) <- colnames(zlm_result@coefC)
-  MAST_result <- lrTest(zlm_result, "clusters")
+  names(MAST_DGE_list) <- colnames(zlm_result@coefC)[3:length(colnames(zlm_result@coefC))]
+  assign("MAST_DGE_array", MAST_DGE_list, env = .GlobalEnv)
 }
 
 # find variable gene co-expression modules
